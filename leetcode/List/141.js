cbdc4728a59687a7c0f06b86cd5f177cf1c4cf16
kongds <kong.ds0308@gmail.com>
2021-07-21 07:52:51 +0800

    feat: 141 环

--- leetcode/List/141.js
+++ leetcode/List/141.js
@@ -12,5 +12,33 @@
  */
 var hasCycle = function (head) {
   // 快慢指针
+  if(head === null){return false}
+  let fast = head.next;
+  let slow = head;
+  while(fast!==null&&fast.next!=null){
+    if(fast ===slow){
+      return true
+    }
+    slow=slow.next;
+    fast = fast.next.next;
+  }
+  return false
+};
+
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+ var hasCycle = function (head) {
   // stack
+  const cycleSet = new Set();
+  while(head){
+    if(cycleSet.has(head)){
+      return true
+    }
+    cycleSet.add(head)
+    head = head.next
+  }
+  return false
 };
